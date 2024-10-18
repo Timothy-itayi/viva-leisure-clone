65e9e432a591c7cd1064f3e520cb8e8ada232eb8
Timothy <timmytam10@gmail.com>
2024-10-18 20:56:53 +1100

    add drawer.tsx to nav

--- hii-republic-clone/app/components/nav/Hamburger.tsx
+++ hii-republic-clone/app/components/nav/Hamburger.tsx
@@ -1,7 +1,7 @@
 const Hamburger = ({ isOpen }: { isOpen: boolean }) => {
     return (
-      <div className="group duration-500   h-20 w-20 rounded-lg">
-        <div className="grid justify-items-center gap-1.5 ">
+      <div className="group duration-500    rounded-lg">
+        <div className="grid justify-items-center  gap-1.5 ">
           <span
             className={`h-1 w-8 rounded-none bg-white transition-all duration-500 ${
               isOpen ? 'rotate-45 translate-y-2.5' : ''
