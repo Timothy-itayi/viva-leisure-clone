844452fe306959a400652e0d5a3cbac226ad2b31
Timothy <timmytam10@gmail.com>
2024-10-19 17:22:56 +1100

    complete loading styles

--- hii-republic-clone/app/info/InfoComponents/Loading.tsx
+++ hii-republic-clone/app/info/InfoComponents/Loading.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Loading: React.FC = () => {
   return (
-    <div className="relative flex flex-col items-center justify-center h-full pt-20"> 
-      <span className="relative font-bold text-3xl text-white mb-4">Loading</span>
+    <div className="flex flex-col items-center justify-center h-full pt-20">
+      <span className="relative font-bold text-3xl text-white mb-10">Loading</span>
       <div className="relative flex items-center justify-center">
-        <div className="h-12 w-12 rounded-full bg-gray-300 animate-orbiting-gray" />
-        <div className="h-12 w-12 rounded-full bg-white animate-orbiting-white" />
+        <div className="h-12 w-12 rounded-full bg-gray-300 animate-pushup-down" />
+        <div className="h-12 w-12 rounded-full bg-white animate-pushup-up" />
       </div>
     </div>
   );
