8970537f9628f71a06d33faac2bf572f6c2b097f
Timothy <timmytam10@gmail.com>
2024-10-19 12:12:15 +1100

    update studios page

--- hii-republic-clone/app/studio/components/card.tsx
+++ hii-republic-clone/app/studio/components/card.tsx
@@ -1,19 +1,39 @@
 import React from 'react';
+import Image from 'next/image';
 
 interface CardProps {
-  title: string; // Type annotation for title
-  address: string; // Type annotation for address
+  title: string;
+  address: string;
+  imageSrc: string;
 }
 
-const Card: React.FC<CardProps> = ({ title, address }) => {
+const Card: React.FC<CardProps> = ({ title, address, imageSrc }) => {
   return (
-    <div className="flex flex-col justify-between  items-center h-full p-6 bg-black text-center  rounded-lg border border-gray-600">
-      <div>
-        <h2 className="text-2xl font-bold mb-2 p-1">
-          <span className="bg-white text-black px-1 mr-2">HR</span>
-          {title}
-        </h2>
-        <p className="mb-4 whitespace-pre-line">{address}</p>
+    <div className="flex flex-col justify-between items-center h-full p-6 bg-black text-center rounded-lg border border-gray-600">
+      <div className="flex flex-col items-center">
+        {/* Flex container for Image and Title */}
+        <div className="flex items-center mb-4">
+          {/* Image */}
+          <div>
+            <Image
+              src={imageSrc}
+              alt="hit logo"
+              width={30}
+              height={30}
+              className="object-cover" // Removed absolute positioning
+            />
+          </div>
+
+          {/* Title */}
+          <h2 className="text-2xl font-bold p-1 text-white ml-2">
+            {title}
+          </h2>
+        </div>
+
+        {/* Address */}
+        <p className="mb-4 whitespace-pre-line">
+          {address}
+        </p>
       </div>
       <button className="border rounded-lg border-[#ff6c3a] text-white font-bold px-4 py-2 mt-auto hover:bg-[#ff6c3a] hover:text-white transition-colors">
         MORE INFO
